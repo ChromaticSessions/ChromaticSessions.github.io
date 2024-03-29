90bf2f194de99576bfbbb3222ab30abec3dfc925
David Martin <dm319@st-andrews.ac.uk>
2024-03-29 21:15:28 +0000

    art now displays images (placeholders for now)

--- chromatic-sessions/components/art.tsx
+++ chromatic-sessions/components/art.tsx
@@ -1,16 +1,56 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Grid from "@mui/material/Grid";
+import { ImageList, ImageListItem } from "@mui/material";
 
-import Wrapper from "./UI/wrapper";
+// import Wrapper from "./UI/wrapper";
 
 export function Art() {
+  let [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    // fetch("")
+    // .then((res) => res.json())
+    // .then((data) => {
+    // setImages(data);
+    // });
+    setImages([
+      "https://source.unsplash.com/1600x900/?art",
+      "https://source.unsplash.com/1600x900/?art",
+      "https://source.unsplash.com/1600x900/?art",
+      "https://source.unsplash.com/1600x900/?art",
+      "https://source.unsplash.com/1600x900/?art",
+      "https://source.unsplash.com/1600x900/?art",
+      "https://source.unsplash.com/600x600/?art",
+      "https://source.unsplash.com/600x600/?art",
+      "https://source.unsplash.com/600x600/?art",
+      "https://source.unsplash.com/600x600/?art",
+      "https://source.unsplash.com/600x600/?art",
+      "https://source.unsplash.com/600x600/?art",
+      "https://source.unsplash.com/600x600/?art",
+      "https://source.unsplash.com/600x600/?art",
+      "https://source.unsplash.com/600x600/?art",
+      "https://source.unsplash.com/600x600/?art",
+    ]);
+  });
+
   return (
-    <Wrapper>
-      <div>
-        <p>Everything is art.</p>
-        <p>Everything.</p>
-        <p>But this art is more visual than most.</p>
-      </div>
-    </Wrapper>
+    // <Grid container spacing={3}>
+    //   <Grid item xs={6}>
+    //     <h1>Art</h1>
+    //     <p>
+    //       Chromatic Sessions is a collection of music, art, and other creative
+    //       works. We are a community of artists and musicians who come together
+    //       to share our work and inspire each other.
+    //     </p>
+    //   </Grid>
+    // </Grid>
+    <ImageList cols={3}>
+      {images.map((image) => (
+        <ImageListItem>
+          <img src={image} />
+        </ImageListItem>
+      ))}
+    </ImageList>
   );
 }
